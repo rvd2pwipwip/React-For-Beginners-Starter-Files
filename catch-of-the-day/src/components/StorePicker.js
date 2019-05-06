680d71bef0fd0a09823e210ae6e07b14df72f62d
Herve Desrosiers <hdesrosiers@mtlt30rjg5l-mac.corp.stingraydigital.com>
2019-05-06 07:59:33 -0400

    first commit -video3 creating our first components

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class StorePicker extends Component {
+  state = {};
+  render() {
+    return <p>I am the store picker!</p>;
+  }
+}
+
+export default StorePicker;
